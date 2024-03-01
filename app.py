2a93995b13303dc4c717b289c9aa1b7d446de4db
graycracker1 <52922421+graycracker1@users.noreply.github.com>
2024-03-01 13:20:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Github'
+    return 'Hello, Github new changes'
 
 # if __name__=='__main__':
 #     app.run(debug=True)
